cccc4f4899232b60cfee66215f6076c6920a0b25
IMASHIKEKULAWALA <it24103453@my.sliit.lk>
2026-04-03 15:32:04 +0530

    Database of the application

---
+++ src/main/java/com/admission/grade1/Grade1Application.java
@@ -0,0 +1,13 @@
+package com.admission.grade1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grade1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grade1Application.class, args);
+	}
+
+}
